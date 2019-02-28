8d35ebe29314baec79bfdfe60d454ba6e1a9faf3
emily baier <emilyrbaier@gmail.com>
2019-02-27 21:54:12 -0800

    tweaked styling, added mediums and animal numbers to each image

--- data/images.js
+++ data/images.js
@@ -3,56 +3,64 @@ const images = [
         'url': 'https://c2.staticflickr.com/4/3195/2894076620_42869680eb_o.jpg',
         'title': 'cityscape',
         'description': 'a block print of a cityscape on green paper',
-        'keyword': 'block print'
+        'medium': 'block print',
+        'animalNumber': 0
     },
 
     {
         'url': 'https://c2.staticflickr.com/4/3145/3076337106_7bffa9f922_b.jpg',
         'title': 'bird',
         'description': 'a gouache painting of a bird with flowers growing out of its back',
-        'keyword': 'gouache'
+        'medium': 'gouache',
+        'animalNumber': 1
     },
 
     {
         'url': 'https://c1.staticflickr.com/3/2490/4253119696_ccd1d032ed_b.jpg',
         'title': 'love house',
         'description': 'a watercolor painting of a house with hearts and foliage',
-        'keyword': 'watercolor'
+        'medium': 'watercolor',
+        'animalNumber': 0
     },
 
     {
         'url': 'https://c1.staticflickr.com/5/4032/4252351259_7529641929_b.jpg',
         'title': 'dinosaur love',
         'description': 'a gouache painting of two royal dinosaurs in love',
-        'keyword': 'gouache'
+        'medium': 'gouache',
+        'animalNumber': 2
     },
 
     {
         'url': 'https://c1.staticflickr.com/3/2731/4531549053_e02536b5af_b.jpg',
         'title': 'nature party',
         'description': 'a mixed-media embroidery of a fox with flowers',
-        'keyword': 'embroidery'
+        'medium': 'embroidery',
+        'animalNumber': 1
     },
 
     {
         'url': 'https://c2.staticflickr.com/2/1422/4609288395_f3af2c211d_b.jpg',
         'title': 'love boat',
         'description': 'a gouache painting of trees with a sailboat floating on thier branches',
-        'keyword': 'gouache'
+        'medium': 'gouache',
+        'animalNumber': 0
     },
 
     {
         'url': 'https://c2.staticflickr.com/4/3362/5840251458_fbe2a5e9a4_b.jpg',
         'title': 'growth spurt 2',
         'description': 'a colored pencil drawing of legs and flowers on blue paper',
-        'keyword': 'colored pencil'
+        'medium': 'colored pencil',
+        'animalNumber': 1
     }, 
 
     {
         'url': 'https://c2.staticflickr.com/4/3148/2708482241_787b229098_b.jpg',
         'title': 'can of worms',
         'description': 'a watercolor painting of a tin can full of mad worms',
-        'keyword': 'watercolor'
+        'medium': 'watercolor',
+        'animalNumber': 3
     }, 
 
 ];
--- src/image-template.js
+++ src/image-template.js
@@ -2,7 +2,10 @@ function makeImageTemplate(image) {
     const html = `
     <li>
     <h2>${image.title}</h2>
+    <p>Medium: ${image.medium}</p>
+    <p class="animal-number"># of Animals: ${image.animalNumber}</p>
     <img src="${image.url}">
+    
     </li>
     `;
     const template = document.createElement('template');
