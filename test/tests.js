00a0e9672a3c5c7288c305f31548a383f4f37700
emily baier <emilyrbaier@gmail.com>
2019-02-27 22:07:55 -0800

    created passing test for number of animals in image

--- test/tests.js
+++ test/tests.js
@@ -5,14 +5,15 @@ const test = QUnit.test;
 function filteredImages(images, filter) {
     return images.filter(image => {
         const hasKeyword = !filter.keyword || image.keyword === filter.keyword;
-        return hasKeyword;
+        const hasAnimals = !filter.animalNumber || image.animalNumber >= filter.animalNumber;
+        return hasKeyword && hasAnimals;
     });
 }
 
 const images = [
-    { title: 'city', keyword: 'watercolor' },
-    { title: 'cloud', keyword: 'gouache' },
-    { title: 'flower', keyword: 'watercolor' }
+    { title: 'city', keyword: 'watercolor', animalNumber: '0' },
+    { title: 'nature party', keyword: 'embroidery', animalNumber: '1' },
+    { title: 'flower', keyword: 'watercolor', animalNumber: '0' }
 ];
 
 test('if keyword watercolor is selected, show only watercolor images', assert => {
@@ -21,8 +22,18 @@ test('if keyword watercolor is selected, show only watercolor images', assert =>
     const filtered = filteredImages(images, filterChoice);
 
     assert.deepEqual(filtered, [
-        { title: 'city', keyword: 'watercolor' },
-        { title: 'flower', keyword: 'watercolor' }
+        { title: 'city', keyword: 'watercolor', animalNumber: '0' },
+        { title: 'flower', keyword: 'watercolor', animalNumber: '0' }
+    ]);
+});
+
+test('if number of animals is 1, return the images with 1 animal', assert => {
+    const filterChoice = { animalNumber: '1' };
+
+    const filtered = filteredImages(images, filterChoice);
+
+    assert.deepEqual(filtered, [
+        { title: 'nature party', keyword: 'embroidery', animalNumber: '1' },
     ]);
 });
 
@@ -31,8 +42,8 @@ test('if no keyword entered, show all images', assert => {
     const filtered = filteredImages(images, filterChoice);
 
     assert.deepEqual(filtered, [
-        { title: 'city', keyword: 'watercolor' },
-        { title: 'cloud', keyword: 'gouache' },
-        { title: 'flower', keyword: 'watercolor' }
+        { title: 'city', keyword: 'watercolor', animalNumber: '0' },
+        { title: 'nature party', keyword: 'embroidery', animalNumber: '1' },
+        { title: 'flower', keyword: 'watercolor', animalNumber: '0' }
     ]);
 });
